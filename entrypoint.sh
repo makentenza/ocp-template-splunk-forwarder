5ee850f2413b534996ffe37ae2664bf328828335
Marcos Entenza <mak@redhat.com>
2016-11-03 09:34:04 +0000

    Move var definition to Build Config and solve issues with NodeSeclector for the DaemonSet

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ if [ "$1" = 'splunk' ]; then
   shift
   ${SPLUNK_BIN} "$@"
 elif [ "$1" = 'start-service' ]; then
-  #$SPLUNK_BIN set deploy-poll $SPLUNK_SERVER --accept-license
-  $SPLUNK_BIN set deploy-poll ${SPLUNK_DEPLOYMENT_SERVER}:8089 --accept-license
+  $SPLUNK_BIN set deploy-poll $SPLUNK_SERVER --accept-license
+  #$SPLUNK_BIN set deploy-poll ${SPLUNK_DEPLOYMENT_SERVER}:8089 --accept-license
   ${SPLUNK_BIN} start --accept-license
   trap "${SPLUNK_BIN} stop" SIGINT SIGTERM EXIT
   tail -n 0 -f ${SPLUNK_HOME}/var/log/splunk/splunkd_stderr.log -f ${SPLUNK_HOME}/var/log/splunk/splunkd.log &
